4a6f9a6d6600bf3447de48ef1b2071f2bd1a33aa
richardsis <richard.moisesleon@gmail.com>
2018-09-29 17:07:37 -0500

    Modificando el proyecto inicial

--- SIGC/src/sigc/SIGC.java
+++ SIGC/src/sigc/SIGC.java
@@ -15,6 +15,8 @@ public class SIGC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
         // TODO code application logic here
     }
     
